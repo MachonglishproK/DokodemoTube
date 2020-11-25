244aa892934c4ae9610330e9b873ba697baf8450
Kakeru <kakeru32ito@gmail.com>
2020-11-25 14:05:13 +0900

    Initial Commit

---
+++ DokodemoTube/AppCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  AppCoordinator.swift
+//  DokodemoTube
+//
+//  Created by 伊藤走 on 11/25/20.
+//
+
+import Foundation
---
+++ DokodemoTube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DokodemoTube
+//
+//  Created by 伊藤走 on 11/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
